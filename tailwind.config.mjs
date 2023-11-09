f6836516eb996ef00e8e065c49ace65d6d448109
codewithxavi <javieradrian.it@gmail.com>
2023-11-09 19:58:54 +0100

    fix bugs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				'poppins': ['Poppins', 'sans-serif']
-			}
+		colors: {
+		  transparent: "transparent",
+		  blue: "#2A4DD0",
+		  green: "#4CA154",
+		  dark1: "#111729",
+		  dark2: "#223344",
+		  dark3: "#909193",
+		  light1: "#F2F9FE",
+		  light2: "#FFFFFF",
 		},
-	},
-	plugins: [],
+		extend: {
+		  flex: {
+			2: "0 0 100%",
+		  },
+		},
+	  },
+	  //darkMode: "class",
+	  plugins: [],
 }
