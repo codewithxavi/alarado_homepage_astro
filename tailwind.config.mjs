efe0a3955ef776448ad54a7ae6af3694b902feca
codewithxavi <javieradrian.it@gmail.com>
2023-11-10 10:46:46 +0100

    fix font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,22 +2,32 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-		  transparent: "transparent",
-		  blue: "#2A4DD0",
-		  green: "#4CA154",
-		  dark1: "#111729",
-		  dark2: "#223344",
-		  dark3: "#909193",
-		  light1: "#F2F9FE",
-		  light2: "#FFFFFF",
-		},
+		// colors: {
+		//   transparent: "transparent",
+		//   blue: "#2A4DD0",
+		//   green: "#4CA154",
+		//   dark1: "#111729",
+		//   dark2: "#223344",
+		//   dark3: "#909193",
+		//   light1: "#F2F9FE",
+		//   light2: "#FFFFFF",
+		// },
 		extend: {
-		  flex: {
-			2: "0 0 100%",
-		  },
+			colors: {
+				transparent: "transparent",
+				blue: "#2A4DD0",
+				green: "#4CA154",
+				dark1: "#111729",
+				dark2: "#223344",
+				dark3: "#909193",
+				light1: "#F2F9FE",
+				light2: "#FFFFFF",
+			},
+			flex: {
+				2: "0 0 100%",
+			},
 		},
-	  },
-	  //darkMode: "class",
-	  plugins: [],
+	},
+	//darkMode: "class",
+	plugins: [],
 }
